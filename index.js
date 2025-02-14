3e69c39f6f9668c0910038777437880f8dbf0637
Wassim Aut <wassimsadoudi11@gmail.com>
2025-02-15 00:24:15 +0100

    update v1

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const con = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 
 });
 
